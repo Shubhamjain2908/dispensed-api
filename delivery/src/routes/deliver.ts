65066c8e5f4cbb9eb571d6739eb39171df4050f5
Shubham Jain <shubhamjain2908@gmail.com>
2020-09-19 15:03:02 +0530

    validating slot

--- delivery/src/routes/deliver.ts
+++ delivery/src/routes/deliver.ts
@@ -1,6 +1,8 @@
-import { validateRequest } from '@tickethub/common';
+import { BadRequestError, validateRequest } from '@tickethub/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import mongoose from 'mongoose';
+import { Slots } from '../models/slot';
 
 const router = express.Router();
 
@@ -15,9 +17,26 @@ router.post(
     ],
     validateRequest,
     async (req: Request, res: Response) => {
+        const slotId = req.query.slotId;
+        await validateSlot(slotId);
+
         res.status(201).send({ title: 'CKMB' });
     }
 );
+
+const validateSlot = async (slotId: any) => {
+    if (!slotId) {
+        throw new BadRequestError('Slot Id is required');
+    }
+    if (!mongoose.Types.ObjectId.isValid(slotId)) {
+        throw new BadRequestError('Not a valid mongoose Id');
+    }
+    const slotExists = await Slots.findOne({ _id: slotId });
+    if (!slotExists) {
+        throw new BadRequestError(`No Slot found for id: ${slotId}`);
+    }
+    return true;
+}
 
 export { router as deliveryRouter };
 
